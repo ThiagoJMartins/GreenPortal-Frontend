91125b54c3a829f9b6b236a539355f51ce587aa7
ThiagoJMartins <thiagodev.martins@gmail.com>
2023-12-31 15:19:39 -0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -10,101 +10,97 @@ import Error from "./components/Error/Error";
 import Form from "./components/Form/Form";
 import Favourites from "./components/Favourites/Favourites";
 import Register from "./components/Register/Register";
+import { BASE_URL } from "./falseENV";
 //!----------------------------------------------------+/
 
 function App() {
-	const URL = "https://greenportal-backend-production.up.railway.app/";
+  const [access, setAccess] = useState(false);
+  const [characters, setCharacters] = useState([]);
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
 
-	const [access, setAccess] = useState(false);
-	const [characters, setCharacters] = useState([]);
-	const { pathname } = useLocation();
-	const navigate = useNavigate();
+  const onSearch = async (id) => {
+    try {
+      const { data } = await axios(`${BASE_URL}/rickandmorty/${id}`);
+      if (data.name) {
+        const characterExists = characters.filter(
+          (char) => char.id === data.id
+        );
+        if (characterExists.length === 0) {
+          setCharacters((oldChars) => [...oldChars, data]);
+        } else {
+          alert("This character has already been added");
+        }
+      }
+    } catch ({ response }) {
+      const { data } = response;
+      console.log(data.error);
+      alert("There is no character with this ID");
+    }
+  };
 
-	const onSearch = async (id) => {
-		try {
-			const { data } = await axios(
-				`https://greenportal-backend-production.up.railway.app/${id}`
-			);
-			if (data.name) {
-				const characterExists = characters.filter(
-					(char) => char.id === data.id
-				);
-				if (characterExists.length === 0) {
-					setCharacters((oldChars) => [...oldChars, data]);
-				} else {
-					alert("This character has already been added");
-				}
-			}
-		} catch ({ response }) {
-			const { data } = response;
-			console.log(data.error);
-			alert("There is no character with this ID");
-		}
-	};
+  const onClose = (id) => {
+    setCharacters(characters.filter((char) => Number(char.id) !== Number(id)));
+  };
 
-	const onClose = (id) => {
-		setCharacters(characters.filter((char) => Number(char.id) !== Number(id)));
-	};
+  const login = async (userData) => {
+    try {
+      const { email, password } = userData;
+      const URL = `${BASE_URL}/rickandmorty/login/`;
+      const { data } = await axios(
+        URL + `?email=${email}&password=${password}`
+      );
+      const { access } = data;
+      setAccess(access);
+      access && navigate("/home");
+    } catch ({ response }) {
+      const { data } = response;
+      alert("Incorrect credentials");
+    }
+  };
 
-	const login = async (userData) => {
-		try {
-			const { email, password } = userData;
-			const URL =
-				"https://greenportal-backend-production.up.railway.app/login/";
-			const { data } = await axios(
-				URL + `?email=${email}&password=${password}`
-			);
-			const { access } = data;
-			setAccess(access);
-			access && navigate("/home");
-		} catch ({ response }) {
-			const { data } = response;
-			alert("Incorrect credentials");
-		}
-	};
+  const register = async (userData) => {
+    try {
+      const URL =
+        "https://greenportal-backend-production.up.railway.app/login/";
+      const { data } = await axios.post(URL, userData);
+      alert("User has been registered successfully");
+      navigate("/");
+    } catch ({ response }) {
+      const { data } = response;
+      alert(data.message);
+    }
+  };
 
-	const register = async (userData) => {
-		try {
-			const URL =
-				"https://greenportal-backend-production.up.railway.app/login/";
-			const { data } = await axios.post(URL, userData);
-			alert("User has been registered successfully");
-			navigate("/");
-		} catch ({ response }) {
-			const { data } = response;
-			alert(data.message);
-		}
-	};
+  function logout() {
+    setAccess(false);
+    navigate("/");
+  }
 
-	function logout() {
-		setAccess(false);
-		navigate("/");
-	}
+  useMemo(() => {
+    !access && navigate("/");
+  }, [access]);
 
-	useMemo(() => {
-		!access && navigate("/");
-	}, [access]);
+  return (
+    <div className="App">
+      {pathname !== "/" && pathname !== "/register" && (
+        <Nav onSearch={onSearch} logout={logout} />
+      )}
 
-	return (
-		<div className="App">
-			{pathname !== "/" && pathname !== "/register" && (
-				<Nav onSearch={onSearch} logout={logout} />
-			)}
-
-			<Routes>
-				<Route
-					path="/home"
-					element={<Cards characters={characters} onClose={onClose} />}
-				/>
-				<Route path="/about" element={<About />} />
-				<Route path="/favourites" element={<Favourites />} />
-				<Route path="/detail/:id" element={<Detail />} />
-				<Route path="*" element={<Error />} />
-				<Route path="/" element={<Form login={login} />} />
-				<Route path="/register" element={<Register register={register} />} />
-			</Routes>
-		</div>
-	);
+      <Routes>
+        <Route
+          path="/home"
+          element={<Cards characters={characters} onClose={onClose} />}
+        />
+        <Route path="/about" element={<About />} />
+        <Route path="/favourites" element={<Favourites />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="*" element={<Error />} />
+        <Route path="/" element={<Form login={login} />} />
+        <Route path="/register" element={<Register register={register} />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/falseENV.js
@@ -0,0 +1,2 @@
+const BASE_URL = "https://greenportal.onrender.com";
+module.exports = { BASE_URL };
