122db8ac7cae11d6c4887626510ed8901475cbbc
ThiagoJMartins <thiagodev.martins@gmail.com>
2023-12-31 15:45:59 -0300

    Update Portal

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -6,69 +6,69 @@ import styles from "./Register.module.scss";
 //!----------------------------------------------------+/
 
 const Register = ({ register }) => {
-	const [userData, setUserData] = useState({
-		email: "",
-		password: "",
-	});
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
 
-	const [errors, setErrors] = useState({});
+  const [errors, setErrors] = useState({});
 
-	const handleChange = (event) => {
-		const property = event.target.name;
-		const value = event.target.value;
+  const handleChange = (event) => {
+    const property = event.target.name;
+    const value = event.target.value;
 
-		setUserData({ ...userData, [property]: value });
+    setUserData({ ...userData, [property]: value });
 
-		setErrors(validate({ ...userData, [property]: value }, setErrors, errors));
-	};
+    setErrors(validate({ ...userData, [property]: value }, setErrors, errors));
+  };
 
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		register(userData);
-	};
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    register(userData);
+  };
 
-	return (
-		<div className={styles.form}>
-			<form className={styles.container}>
-				<div>
-					<img
-						className={styles.logo}
-						src="./portalB.png"
-						alt="RegisterPortal"
-					/>
-				</div>
-				<label>Email</label>
-				<input
-					className={styles.entries}
-					placeholder="Email"
-					type="text"
-					value={userData.email}
-					name="email"
-					onChange={handleChange}
-				/>
-				<span className={styles.warning}>{errors.email}</span>
+  return (
+    <div className={styles.form}>
+      <form className={styles.container}>
+        <div>
+          <img
+            className={styles.logo}
+            src="../../../public/portalB.png"
+            alt="RegisterPortal"
+          />
+        </div>
+        <label>Email</label>
+        <input
+          className={styles.entries}
+          placeholder="Email"
+          type="text"
+          value={userData.email}
+          name="email"
+          onChange={handleChange}
+        />
+        <span className={styles.warning}>{errors.email}</span>
 
-				<label>Password</label>
-				<input
-					className={styles.entries}
-					placeholder="Password"
-					type="password"
-					value={userData.password}
-					name="password"
-					onChange={handleChange}
-				/>
-				<span className={styles.warning}>{errors.password}</span>
-				<br />
-				<button className={styles.boton} type="submit" onClick={handleSubmit}>
-					Signup
-				</button>
-				<br />
-				<Link to="/" className={styles.linkNav}>
-					<span className={styles.linkLogin}>Login</span>
-				</Link>
-			</form>
-		</div>
-	);
+        <label>Password</label>
+        <input
+          className={styles.entries}
+          placeholder="Password"
+          type="password"
+          value={userData.password}
+          name="password"
+          onChange={handleChange}
+        />
+        <span className={styles.warning}>{errors.password}</span>
+        <br />
+        <button className={styles.boton} type="submit" onClick={handleSubmit}>
+          Signup
+        </button>
+        <br />
+        <Link to="/" className={styles.linkNav}>
+          <span className={styles.linkLogin}>Login</span>
+        </Link>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
