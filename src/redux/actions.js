bf803fb4f3d9250ba0a874a3c292e702d6b9214e
ThiagoJMartins <thiagodev.martins@gmail.com>
2024-01-07 01:00:27 -0300

    Fav Update

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,69 +1,69 @@
 import axios from "axios";
 import {
-	ADD_FAV,
-	REMOVE_FAV,
-	FILTER_CARDS,
-	ORDER_CARDS,
-	GET_FAV,
+  ADD_FAV,
+  REMOVE_FAV,
+  FILTER_CARDS,
+  ORDER_CARDS,
+  GET_FAV,
 } from "./action-types";
 //!----------------------------------------------------+/
 
-const URL = "https://greenportal-backend-production.up.railway.app/fav";
+const URL = "https://greenportal.onrender.com/fav";
 
 const addFav = (character) => {
-	return async (dispatch) => {
-		try {
-			const { data } = await axios.post(URL, character);
-			return dispatch({
-				type: ADD_FAV,
-				payload: data,
-			});
-		} catch (error) {
-			console.error(error);
-		}
-	};
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.post(URL, character);
+      return dispatch({
+        type: ADD_FAV,
+        payload: data,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 };
 
 const removeFav = (id) => {
-	return async (dispatch) => {
-		try {
-			const { data } = await axios.delete(`${URL}/${id}`);
-			return dispatch({
-				type: REMOVE_FAV,
-				payload: data,
-			});
-		} catch (error) {
-			console.error(error);
-		}
-	};
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.delete(`${URL}/${id}`);
+      return dispatch({
+        type: REMOVE_FAV,
+        payload: data,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 };
 
 const filterCards = (gender) => {
-	return {
-		type: FILTER_CARDS,
-		payload: gender,
-	};
+  return {
+    type: FILTER_CARDS,
+    payload: gender,
+  };
 };
 
 const orderCards = (order) => {
-	return {
-		type: ORDER_CARDS,
-		payload: order,
-	};
+  return {
+    type: ORDER_CARDS,
+    payload: order,
+  };
 };
 
 const getFav = () => {
-	return async (dispatch) => {
-		try {
-			const { data } = await axios.get(URL);
-			return dispatch({
-				type: GET_FAV,
-				payload: data,
-			});
-		} catch (error) {
-			console.log(error);
-		}
-	};
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get(URL);
+      return dispatch({
+        type: GET_FAV,
+        payload: data,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 };
 
 export { addFav, removeFav, filterCards, orderCards, getFav };
