1a63d134f2acc2c851c1c5a7b135cc8d5d4693ca
ThiagoJMartins <thiagodev.martins@gmail.com>
2024-01-07 15:37:07 -0300

    Responsive design

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -6,43 +6,46 @@ import styles from "./Nav.module.scss";
 //!----------------------------------------------------+/
 
 export default function Nav(props) {
-	return (
-		<div className={styles.navContainer}>
-			<div className={styles.logo}>
-				<Link to={"/home"}>
-					<img src={Logo} alt="logo" width={200} />
-				</Link>
-			</div>
+  return (
+    <div className={styles.navContainer}>
+      <div className={styles.logo}>
+        <Link to={"/home"}>
+          <img src={Logo} alt="logo" width={200} />
+        </Link>
+      </div>
 
-			<div className={styles.secondSection}>
-				<div className={styles.wrapperItems1}>
-					<Link to={"/home"} className={styles.linkNav}>
-						<span className={styles.itemNav} id="home">
-							Home
-						</span>
-					</Link>
+      <div className={styles.secondSection}>
+        <div className={styles.searchNav}>
+          <SearchBar onSearch={props.onSearch} />
+        </div>
 
-					<Link to={"/favourites"} className={styles.linkNav}>
-						<span className={styles.itemNav} id="logout">
-							Favs
-						</span>
-					</Link>
-				</div>
+        <div className={styles.wrapperContainer}>
+          <div className={styles.wrapperItems1}>
+            <Link to={"/home"} className={styles.linkNav}>
+              <span className={styles.itemNav} id="home">
+                Home
+              </span>
+            </Link>
 
-				<div className={styles.searchNav}>
-					<SearchBar onSearch={props.onSearch} />
-				</div>
-				<div className={styles.wrapperItems2}>
-					<Link to={"/about"} className={styles.linkNav}>
-						<span className={styles.itemNav} id="about">
-							About
-						</span>
-					</Link>
-					<span onClick={props.logout} className={styles.itemNav} id="logout">
-						Logout
-					</span>
-				</div>
-			</div>
-		</div>
-	);
+            <Link to={"/favourites"} className={styles.linkNav}>
+              <span className={styles.itemNav} id="logout">
+                Favs
+              </span>
+            </Link>
+          </div>
+
+          <div className={styles.wrapperItems2}>
+            <Link to={"/about"} className={styles.linkNav}>
+              <span className={styles.itemNav} id="about">
+                About
+              </span>
+            </Link>
+            <span onClick={props.logout} className={styles.itemNav} id="logout">
+              Logout
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
